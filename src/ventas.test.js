99325babe27a44f710e7af230257a5a81fa8022a
Arturo10021 <arturo.guzman@ucb.edu.bo>
2023-08-30 14:11:39 -0400

    Mostrar el precio total con el Porcentaje de descuento que corresponde a 3000 y Mostrar el valor de porcentaje del impuesto

--- src/ventas.test.js
+++ src/ventas.test.js
@@ -31,7 +31,17 @@ describe('Ventas', () => {
         expect(ventas.calcularImpuesto(125, 0.0825)).toEqual(10.3125);
     });
 
-    it("mostrar precio final con descuento para total menor o igual a 1000(3%)", () => {
+    it("Mostrar el precio total con el Porcentaje de descuento que corresponde a 1000", () => {
+        let ventas = new Ventas();
+        expect(ventas.precioFinal(1000, 0.03)).toEqual(970);
+    });
+
+    it("Mostrar el precio total con el Porcentaje de descuento que corresponde a 3000", () => {
+        let ventas = new Ventas();
+        expect(ventas.precioFinal(3000, 0.05)).toEqual(2850);
+    });
+
+    it("Mostrar el valor del porcentaje del descuento", () => {
         let ventas = new Ventas();
         expect(ventas.precioFinal(1000, 0.03)).toEqual(970);
     });
