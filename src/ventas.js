0952a1d842c602e0d90c193e4bd33ee054a2daeb
Arturo10021 <arturo.guzman@ucb.edu.bo>
2023-08-30 13:24:04 -0400

    Mostrar el precio por item

--- src/ventas.js
+++ src/ventas.js
@@ -2,6 +2,10 @@ export class Ventas {
     cantidadItems(cantidad) {
         return cantidad;
     }
+
+    precioPorItem(precio) {
+        return precio;
+    }
 }
 
 export default Ventas;
--- src/ventas.test.js
+++ src/ventas.test.js
@@ -5,4 +5,9 @@ describe('Ventas', () => {
         const ventas = new Ventas();
         expect(ventas.cantidadItems(50)).toBe(50);
     });
+
+    it("Mostrar el precio por item", () => {
+        const ventas = new Ventas();
+        expect(ventas.precioPorItem(100)).toBe(100);
+    });
 });    
